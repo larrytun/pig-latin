e40f86a38e635552bcf53d43df1a1856775eb5fc
Angie Smith and Larry Tunseth <larry@>
2016-08-17 14:42:02 -0700

    move any consonants at the beginning of a word until there is a vowel to the end of the word and adds ay.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,16 @@ var vowels = ["a","e","i","o","u"]
 
 var translation = function(userInput) {
   if (isNaN(userInput)) {
-    if ((vowels).indexOf(userInput[1])) {
-
+    console.log(userInput[0]);
+    if ((vowels).includes(userInput[0])) {
+      userInput.push("a","y");
+    } else {
+      for (i = 0; !(vowels).includes(userInput[i]);) {
+        userInput.push(userInput[0]);
+        userInput.splice(0,(i+1));
+        console.log(userInput)
+        console.log(i)
+      }
       userInput.push("a","y");
     }
   }
