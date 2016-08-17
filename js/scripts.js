c05879824e704f9e377e5f7daf5590d388a89e72
Angie Smith and Larry Tunseth <angie@>
2016-08-17 13:21:27 -0700

    add ay to the end of the user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
 // business
 var vowels = ["a","e","i","o","u"]
+
 var translation = function(userInput) {
   if (isNaN(userInput)) {
-    for (var i = 0; i < userInput.length; i += 1 ) {
-      if ((vowels).indexOf(userInput[1])) {
-        alert("vowel");
-      }
+    if ((vowels).indexOf(userInput[1])) {
+
+      userInput.push("a","y");
     }
   }
-
 };
-
 
 // user
 $(document).ready(function() {
   $("form").submit(function(event) {
     var userInput = $("#input").val().split("");
     var result = translation(userInput);
+    userInput = userInput.join("");
     $(".output").text(userInput);
     event.preventDefault();
 
